2649f7d4d188f9fe8aa63ff8576ba6f213435395
jonghoon lee <jonghoon7431@gmail.com>
2024-05-31 10:06:06 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyle from './components/globalstyle/GlobalStyle';
+import Router from './shared/Router';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
+}
+
+export default App;
