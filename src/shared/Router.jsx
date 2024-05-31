86759e6bc5805404bbf8c6598337d691a556c17c
jonghoon lee <jonghoon7431@gmail.com>
2024-05-31 19:05:13 +0900

    셋업 확인

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
 import Layout from '../components/Layout.jsx';
+import CreatePost from '../pages/CreatePost';
+import Home from '../pages/Home';
+import LogIn from '../pages/LogIn';
+import MyPage from '../pages/MyPage';
+import Post from '../pages/Post';
+import SignIn from '../pages/SignIn';
 
 const Router = () => {
   return (
@@ -9,6 +14,11 @@ const Router = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/create_post" element={<CreatePost />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/my_page" element={<MyPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
