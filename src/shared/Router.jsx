6c623fc454c884b64891c33f57cb792bde5de85a
reeny404 <happy2hyeon@kakao.com>
2024-05-31 20:13:41 +0900

    router 변경할 때 누락했떤 layout 복구

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout';
 import CreatePost from '../pages/CreatePost';
 import Home from '../pages/Home';
 import LogIn from '../pages/LogIn';
@@ -8,28 +9,33 @@ import SignIn from '../pages/SignIn';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/login',
-    element: <LogIn />,
-  },
-  {
-    path: '/sign_in',
-    element: <SignIn />,
-  },
-  {
-    path: '/create_post',
-    element: <CreatePost />,
-  },
-  {
-    path: '/post/:id',
-    element: <Post />,
-  },
-  {
-    path: '/my_page',
-    element: <MyPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <LogIn />,
+      },
+      {
+        path: '/sign_in',
+        element: <SignIn />,
+      },
+      {
+        path: '/create_post',
+        element: <CreatePost />,
+      },
+      {
+        path: '/post/:id',
+        element: <Post />,
+      },
+      {
+        path: '/my_page',
+        element: <MyPage />,
+      },
+    ],
   },
 ]);
 
