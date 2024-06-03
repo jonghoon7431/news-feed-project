2a240c5341ba2e89170a62f8370d48134046364d
hyowls <hhvucyh@gmail.com>
2024-06-03 16:50:48 +0900

    update : create post CSS

--- src/components/createpost/Form.jsx
+++ src/components/createpost/Form.jsx
@@ -7,22 +7,19 @@ function Form() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [tag, setTag] = useState('');
-
+  
   const handleSubmit = async(e) => {
     e.preventDefault()
-    // 데이터 삽입 처리
     const { data, error } = await supabase
       .from('POSTS')
       .insert([
         { name, title, content, tag }
       ])
-
     if (error) {
       console.error('Error inserting data:', error)
     } else {
       console.log('Data inserted:', data)
       alert('Post added successfully!')
-      // 폼 초기화
       setName('')
       setTitle('')
       setContent('')
@@ -32,21 +29,24 @@ function Form() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} >
-        <input type="text" placeholder='작성자 이름' value={name} onChange={(e) => {
+      <form onSubmit={handleSubmit} className='flex-column' >
+        <div style={{height:'40px', display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+          <button className='' style={{margin:'10px 20px 0'}}  type='button'>뒤로가기</button>
+          <button className='' style={{margin:'10px 20px 0 0'}}  type='submit'>저장</button>
+        </div>
+        <input style={{padding : '15px', fontSize:'18px', margin:'10px 0'}} type="text" placeholder='작성자 이름' value={name} onChange={(e) => {
           setName(e.target.value)
         }} />
-        <input  type="text" placeholder='제목' value={title} onChange={(e) => {
+        <input className='' style={{padding : '15px', fontSize:'18px', margin:'0 0 10px'}}  type="text" placeholder='제목' value={title} onChange={(e) => {
           setTitle(e.target.value)
         }}/>
-        <textarea  placeholder='내용' value={content} onChange={(e) => {
+        <textarea className=''  style={{resize:'none', width:'100%', height:'300px', padding:'15px', fontSize:'16px', margin :'0 0 10px'}} placeholder='내용' value={content} onChange={(e) => {
           setContent(e.target.value)
         }}></textarea>
-        <input type="text"  placeholder='#해시태그' value={tag} onChange={(e) => {
+        <input className='' style={{width:'30%', padding:'10px'}}  type="text"  placeholder='#해시태그' value={tag} onChange={(e) => {
           setTag(e.target.value)
         }} />
-        <button type='button'>태그 등록</button>
-        <button type='submit'>저장</button>
+        <button className=''  type='button'>태그 등록</button>
       </form>
     </div>
   )
