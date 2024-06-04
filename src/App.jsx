5483a5d9e12afc474b2b9d3fe8579f4bb6677c04
eldoradodo <rla1eh2dus3@naver.com>
2024-06-04 20:36:38 +0900

    프로바이더 위치 변경, onAuthStateChange 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 // src/App.jsx
 
-import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './components/globalstyle/GlobalStyle';
 import router from './shared/Router'; // router 임포트
-import store from './redux/config/configStore';
+import { useEffect } from 'react';
+import supabase from './supabaseClient';
+
+
 
 function App() {
+
+  useEffect(() => {
+    const subscription = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log(event, session)
+        if (event === 'SIGNED_OUT') {
+          // setSession(null)
+        } else if (session) {
+          // setSession(session)
+        }
+      })
+  
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+  
+
   return (
-    <Provider store={store}>
-      <>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </>
-    </Provider>
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
   );
 }
 
