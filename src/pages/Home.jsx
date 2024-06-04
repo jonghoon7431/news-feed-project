79342fb5277175684c7fb6aaa32a5ecb65041cf6
reeny404 <happy2hyeon@kakao.com>
2024-06-04 15:16:34 +0900

    feat: header를 layout에 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,9 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api/api';
 import pencil from '../assets/pencil.png';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 import PostList from '../components/PostList';
 import { SearchSection } from '../components/Search';
 
-const isLoggedIn = true; // 임시 변수
 function Home() {
   const [searchedPosts, setSearchedPosts] = useState([]); // 검색 결과
   const [recentPosts, setRecentPosts] = useState([]);
@@ -15,13 +12,8 @@ function Home() {
 
   useEffect(() => {
     (async () => {
-      const list = await api.posts.getPopularPosts();
-      setPopularPosts(list);
-    })();
-
-    (async () => {
-      const list = await api.posts.getRecentPosts();
-      setRecentPosts(list);
+      setPopularPosts(await api.posts.getPopularPosts());
+      setRecentPosts(await api.posts.getRecentPosts());
     })();
   }, []);
 
@@ -32,22 +24,6 @@ function Home() {
 
   return (
     <>
-      <Header>
-        {isLoggedIn ? (
-          <>
-            <Link to="/sign_in" className="py-1 px-2 rounded text-sm font-bold">
-              회원가입
-            </Link>
-            <Link to="/login" className="py-1 px-2 rounded text-sm font-bold">
-              로그인
-            </Link>
-          </>
-        ) : (
-          <Link to="/my_page" className="py-1 px-2 rounded text-sm font-bold">
-            마이페이지
-          </Link>
-        )}
-      </Header>
       <article>
         <SearchSection handleSearch={handleSearch} />
         <section>
@@ -61,7 +37,6 @@ function Home() {
           )}
         </section>
       </article>
-      <Footer></Footer>
       <Link
         to="/create_post"
         className="fixed right-0 bottom-0 m-2 rounded w-10 h-10 bg-gray-300 flex justify-center items-center hover:bg-white"
