ed8b2f24459df1382c925a7875a189fa9c4c2f4a
reeny404 <happy2hyeon@kakao.com>
2024-06-04 15:15:54 +0900

    feat: header를 layout에 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import Footer from './Footer';
+import Header from './Header';
+
+const isLoggedIn = false; // 임시 변수
 
 function Layout() {
   return (
     <Main>
+      {location.pathname === '/' ? (
+        <Header>
+          <HomeButtons isLogIn={isLoggedIn} />
+        </Header>
+      ) : (
+        <Header />
+      )}
       <Outlet />
+      <Footer />
     </Main>
   );
 }
@@ -14,5 +26,26 @@ const Main = styled.main`
   max-width: 1000px;
   margin: 0 auto;
 `;
+
+function HomeButtons({ isLogIn }) {
+  if (isLogIn) {
+    return (
+      <Link to="/my_page" className="py-1 px-2 rounded text-sm font-bold">
+        마이페이지
+      </Link>
+    );
+  }
+
+  return (
+    <>
+      <Link to="/sign_in" className="py-1 px-2 rounded text-sm font-bold">
+        회원가입
+      </Link>
+      <Link to="/login" className="py-1 px-2 rounded text-sm font-bold">
+        로그인
+      </Link>
+    </>
+  );
+}
 
 export default Layout;
