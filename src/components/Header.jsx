2c49dabafeb5358ede7b859c33967ff17a2d188f
eldoradodo <rla1eh2dus3@naver.com>
2024-06-05 12:49:15 +0900

    로그인 회원가입 페이지에서 오른쪽 상단 버튼 안뜨도록 헤더 일부 수정 / 로그인 회원가입 창 tailwindcss 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import mvLogo from '../assets/logoMV.svg';
 
-function Header({ children }) {
+function Header({ showAuthLinks = true }) {
   return (
     <header className="flex justify-between h-16 px-3 py-5 items-center">
       <span className="h-full ml-2">
@@ -9,7 +9,16 @@ function Header({ children }) {
           <img src={mvLogo} alt="MV logo" className="h-full" />
         </Link>
       </span>
-      <span>{children}</span>
+      {showAuthLinks && (
+        <span>
+          <Link to="/sign_up" className="mr-4 text-lg  hover:underline">
+            회원가입
+          </Link>
+          <Link to="/login" className="text-lg  hover:underline">
+            로그인
+          </Link>
+        </span>
+      )}
     </header>
   );
 }
