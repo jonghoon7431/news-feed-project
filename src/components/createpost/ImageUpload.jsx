30a3c3e79063be5c8e693df17154576efb5bb8b0
hyowls <hhvucyh@gmail.com>
2024-06-05 16:28:08 +0900

    hover추가 및 업로드 버튼 색 수정

--- src/components/createpost/ImageUpload.jsx
+++ src/components/createpost/ImageUpload.jsx
@@ -7,7 +7,7 @@ const ImageUpload = ({ handleImageChange }) => {
   return (
     <section className='mx-4 my-4'>
       <div className='flex justify-end'>
-        <label htmlFor="image-upload" className='cursor-pointer bg-slate-300 p-2.5 rounded-lg'>이미지 업로드</label>
+        <label htmlFor="image-upload" className='cursor-pointer bg-white p-2.5 rounded-lg hover:bg-slate-300 hover:text-white hover:duration-200'>이미지 업로드</label>
         <input className='hidden' id='image-upload' type="file" accept='image/*' multiple onChange={handleImageChange} />
       </div>
       <div className='mt-6 flex flex-row flex-wrap justify-end'>
