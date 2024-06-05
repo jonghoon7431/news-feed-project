9d291ac34d0b969b1801c7e4daea7029915e61f6
hyowls <hhvucyh@gmail.com>
2024-06-05 20:12:34 +0900

    이미지 업로드 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,17 +1,80 @@
 import styled from 'styled-components';
 import PostItem from '../components/PostItem';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import supabase from '../supabaseClient';
 
 function MyPage() {
+  const [profileImg, setProfileImg] = useState([]);
+  const [userId, setUserId] = useState('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        setUserId(user.id);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  const handleProfileImage = async (e) => {
+    e.preventDefault();
+    const uploadedProfile = [];
+    for (let profile of profileImg) {
+      const fileExt = profile.name.split('.').pop();
+      const fileName = `${Date.now()}-${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+
+      const { error: storageError } = await supabase.storage
+        .from('profileimg')
+        .upload(filePath, profile);
+
+      if (storageError) {
+        alert('잘못된 접근입니다');
+        return;
+      } else {
+        const { data, error: urlError } = supabase.storage
+          .from('profileimg')
+          .getPublicUrl(filePath);
+
+        if (urlError) {
+          alert('잘못된 접근입니다');
+          return;
+        }
+        uploadedProfile.push(data.publicUrl);
+      }
+    }
+    const profileUrlsString = JSON.stringify(uploadedProfile);
+
+    const { error } = await supabase
+      .from('USER_PROFILE')
+      .insert([
+        { profile_url: profileUrlsString, user_id: userId }
+      ]);
+
+    if (error) {
+      alert('잘못된 접근입니다');
+    } else {
+      alert('저장이 완료되었습니다');
+      setProfileImg([]);
+    }
+  }
+
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    const newImage = [...profileImg, ...files];
+    setProfileImg(newImage);
+  }
+
   return (
     <MyPageCon>
       <MyPageArea>
-        <ProfileArea>
+        <ProfileArea onSubmit={handleProfileImage}>
           <ProfileIcon>
             <label htmlFor="profileFileUpload" id="profileFileUploadBtn">
               파일 업로드
             </label>
-            <input type="file" id="profileFileUpload" style={{ display: 'none' }} />
+            <input type="file" id="profileFileUpload" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} />
           </ProfileIcon>
           <ProfileInfo>
             <ProfileId>아이디abc</ProfileId>
@@ -34,6 +97,7 @@ function MyPage() {
   );
 }
 
+
 // 라벨은 테일윈드로 원하는 디자인 처리
 
 const MyPageCon = styled.div`
@@ -51,7 +115,7 @@ const MyPageArea = styled.div`
   background-color: darkgray;
 `;
 
-const ProfileArea = styled.div`
+const ProfileArea = styled.form`
   min-width: 100%;
   max-width: 100%;
   display: flex;
