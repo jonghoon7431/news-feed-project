be9473e32cd88c8af806c8e5148c3169429ffae1
minseo <mensung6@gmail.com>
2024-06-06 19:57:20 +0900

    dev 브랜치 수정사항 합침

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
 import api from '../api/api';
 import DefaultProfileUrl from '../assets/profile.png';
 import PostList from '../components/PostList';
